41f607be69fbf78e3a790d53bde951a9e0929eb4
julian1 <julian.atkinson71@gmail.com>
2016-06-08 11:47:31 +1000

    working

--- upload.sh
+++ upload.sh
@@ -1,16 +1,11 @@
 #!/bin/bash -x
 
 aws s3 sync ../s3-static-site/ s3://schemas.aodn.org.au \
-    --exclude=".*"  --exclude="*.git/*"  --exclude="upload.sh"  \
+    --exclude=".*" --exclude="*.git/*" --exclude="upload.sh" \
     --acl public-read
 
 
 # \ --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
-
-
 #  --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers full=emailaddress=user@example.com
-
-
-
 
 
