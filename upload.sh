3ab60b78bba2521d9c98c94048d4857926b887d7
julian1 <julian.atkinson71@gmail.com>
2016-06-08 12:09:55 +1000

    tidy upload

--- upload.sh
+++ upload.sh
@@ -2,7 +2,7 @@
 
 # does much better at mime type association than the gui tools
 
-aws s3 sync ../s3-static-site/ s3://schemas.aodn.org.au \
+aws s3 sync ./ s3://schemas.aodn.org.au \
     --exclude=".*" --exclude="*.git/*" --exclude="upload.sh" \
     --acl public-read
 
